118e1c4fc79a27a5d891453f1213c4df40ef06f0
= <=>
2023-05-01 20:37:56 +0700

    [DEV][Thang]-[add config routeloactor and log]

---
+++ src/main/java/com/microservices/apigateway/config/ApiGatewayConfiguration.java
@@ -0,0 +1,27 @@
+package com.microservices.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(p -> p.path("/client00/**").uri("lb://client00"))
+                .route(p -> p.path("/client01/**")
+                        .filters(f -> f.rewritePath("/client01/(?<segment>.*)","/${segment}"))
+                        .uri("lb://client01"))
+                .route(p -> p.path("/client00/**")
+                        .filters(
+                                f -> f.rewritePath(
+                                        "/client00-new/(?<segment>.*)",
+                                        "/client00-feign/${segment}"
+                                )
+                        ).uri("lb://client00")
+                )
+                .build();
+    }
+}
