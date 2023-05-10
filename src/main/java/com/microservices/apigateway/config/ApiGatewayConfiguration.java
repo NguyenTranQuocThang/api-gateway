7a3b57f5b325bb2e6aa754b437a94ec04b0c1acf
= <=>
2023-05-10 21:03:04 +0700

    [DEV][Thang]-[add config for docker]

--- src/main/java/com/microservices/apigateway/config/ApiGatewayConfiguration.java
+++ src/main/java/com/microservices/apigateway/config/ApiGatewayConfiguration.java
@@ -10,18 +10,21 @@ public class ApiGatewayConfiguration {
     @Bean
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
         return builder.routes()
-                .route(p -> p.path("/client00/**").uri("lb://client00"))
+//                .route(p -> p.path("/client00/**").uri("lb://client00"))
                 .route(p -> p.path("/client01/**")
                         .filters(f -> f.rewritePath("/client01/(?<segment>.*)","/${segment}"))
                         .uri("lb://client01"))
+//                .route(p -> p.path("/client00/**")
+//                        .filters(
+//                                f -> f.rewritePath(
+//                                        "/client00-new/(?<segment>.*)",
+//                                        "/client00-feign/${segment}"
+//                                )
+//                        ).uri("lb://client00")
+//                )
                 .route(p -> p.path("/client00/**")
-                        .filters(
-                                f -> f.rewritePath(
-                                        "/client00-new/(?<segment>.*)",
-                                        "/client00-feign/${segment}"
-                                )
-                        ).uri("lb://client00")
-                )
+                        .filters(f -> f.rewritePath("/client00/(?<segment>.*)","/${segment}"))
+                        .uri("lb://client00"))
                 .build();
     }
 }
